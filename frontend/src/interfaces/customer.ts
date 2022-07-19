3cb3a4af1d8ebd27515b4f246dc5053c0a2c76d8
Ronier Ramos <ronier2332@gmail.com>
2022-07-19 14:12:18 +0200

    added policies list to table

---
+++ frontend/src/interfaces/customer.ts
@@ -0,0 +1,30 @@
+export interface IPolicies {
+  id: string;
+  customer: ICustomer;
+  provider: string;
+  insuranceType: InsuranceType;
+  status: PolicyStatus;
+  startDate: Date;
+  endDate: Date;
+  createdAt: Date;
+}
+
+export interface ICustomer {
+  id: string;
+  firstName: string;
+  lastName: string;
+  dateOfBirth: Date;
+}
+
+export enum InsuranceType {
+  LIABILITY = "LIABILITY",
+  HOUSEHOLD = "HOUSEHOLD",
+  HEALTH = "HEALTH",
+}
+
+export enum PolicyStatus {
+  ACTIVE = "ACTIVE",
+  PENDING = "PENDING",
+  CANCELLED = "CANCELLED",
+  DROPPED_OUT = "DROPPED_OUT",
+}
