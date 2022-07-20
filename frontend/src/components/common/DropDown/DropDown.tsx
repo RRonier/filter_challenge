a73005f115e876d1781dfdfc9ff3d9c05a3700e3
Ronier Ramos <ronier2332@gmail.com>
2022-07-20 05:59:00 +0200

    created custom dropdown component

---
+++ frontend/src/components/common/DropDown/DropDown.tsx
@@ -0,0 +1,56 @@
+import { DropDownProps } from "../../../types/types";
+
+const DropDown = ({
+  value,
+  onHandleChange,
+  providersList,
+  insurancesList,
+  name,
+  placeholder,
+  text,
+}: DropDownProps) => (
+  <div className="flex justify-start">
+    <div className="mb-3 xl:w-96">
+      <select
+        name={name}
+        value={value}
+        onChange={onHandleChange}
+        placeholder={placeholder}
+        className="form-select appearance-none
+            block
+            w-full
+            px-3
+            py-1.5
+            text-base
+            font-normal
+            text-gray-700
+            bg-white bg-clip-padding bg-no-repeat
+            border border-solid border-gray-300
+            rounded
+            transition
+            ease-in-out
+            m-0
+            focus:text-gray-700 focus:bg-white
+            focus:border-blue-600 focus:outline-none"
+        aria-label="Default select example"
+      >
+        <option value="">{text}</option>
+        {providersList
+          ? providersList.map(({ provider }, key) => (
+              <option key={key} value={provider}>
+                {provider}
+              </option>
+            ))
+          : insurancesList
+          ? insurancesList.map(({ insuranceType }, key) => (
+              <option key={key} value={insuranceType}>
+                {insuranceType}
+              </option>
+            ))
+          : null}
+      </select>
+    </div>
+  </div>
+);
+
+export default DropDown;
