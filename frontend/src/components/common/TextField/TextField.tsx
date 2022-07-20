05ee3ebac6a0c7bae8b69cda2c869d58e5278543
Ronier Ramos <ronier2332@gmail.com>
2022-07-20 05:59:14 +0200

    created custom textfield component

---
+++ frontend/src/components/common/TextField/TextField.tsx
@@ -0,0 +1,33 @@
+import { TextFieldProps } from "../../../types/types";
+
+export const TextField = ({ onHandleChange, params }: TextFieldProps) => (
+  <div className="flex justify-start">
+    <div className="mb-3 xl:w-96">
+      <input
+        type="search"
+        name="search"
+        onChange={onHandleChange}
+        value={params.search}
+        className="
+            form-control
+            block
+            w-full  
+            px-3
+            py-1.5
+            text-base
+            font-normal
+            text-gray-700
+            bg-white bg-clip-padding
+            border border-solid border-gray-300
+            rounded
+            transition
+            ease-in-out
+            m-0
+            focus:text-gray-700 focus:bg-white 
+            focus:border-blue-600 focus:outline-none"
+        id="exampleSearch2"
+        placeholder="Search by name"
+      />
+    </div>
+  </div>
+);
